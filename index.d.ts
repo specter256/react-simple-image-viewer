382e725d051fc360c60c4ff27fd9777ed487f327
Alexander Tarasov <int3rrup7@gmail.com>
2019-09-07 13:17:21 +0300

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export interface IProps extends React.Props<ReactSimpleImageViewer> {
+  src: string[];
+  currentIndex?: number;
+  onClose: () => void;
+}
+
+export interface IState {
+  currentIndex: number;
+}
+
+declare class ReactSimpleImageViewer extends React.Component<IProps, IState> {
+}
+
+declare module 'react-simple-image-viewer' {
+}
+
+export default ReactSimpleImageViewer;
---
+++ src/ReactSimpleImageViewer.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import './ReactSimpleImageViewer.css';
+
+interface IProps {
+  src: string[];
+  currentIndex?: number;
+  onClose: () => void;
+}
+
+interface IState {
+  currentIndex: number;
+}
+
+export default class ReactSimpleImageViewer extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleWheel = this.handleWheel.bind(this);
+    this.callOnClose = this.callOnClose.bind(this);
+
+    document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener('wheel', this.handleWheel);
+
+    this.state = {
+      currentIndex: this.props.currentIndex === undefined
+        ? 0
+        : this.props.currentIndex,
+    };
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+    document.removeEventListener('wheel', this.handleWheel);
+  }
+
+  private changeImage(direction: number) {
+    let nextIndex = this.state.currentIndex + direction;
+
+    if (nextIndex > this.props.src.length - 1) {
+      nextIndex = 0;
+    } else if (nextIndex < 0) {
+      nextIndex = this.props.src.length - 1;
+    }
+
+    this.setState({
+      currentIndex: nextIndex,
+    });
+  }
+
+  private callOnClose() {
+    if (this.props.onClose !== undefined) {
+      this.props.onClose();
+    }
+  }
+
+  private handleKeyDown(event: any) {
+    if (event.key === 'Escape') {
+      this.callOnClose();
+    }
+
+    if (['ArrowLeft', 'h'].includes(event.key)) {
+      this.changeImage(-1);
+    }
+
+    if (['ArrowRight', 'l'].includes(event.key)) {
+      this.changeImage(1);
+    }
+  }
+
+  private handleClick(event: any) {
+    if (event.target && event.target.id === 'ImageViewer') {
+      this.callOnClose();
+    }
+  }
+
+  private handleWheel(event: any) {
+    if (event.wheelDeltaY > 0) {
+      this.changeImage(-1);
+    } else {
+      this.changeImage(1);
+    }
+  }
+
+  render() {
+    const { src } = this.props;
+    const { currentIndex } = this.state;
+
+    return (
+      <div
+        id="ImageViewer"
+        className="modal"
+        onKeyDown={this.handleKeyDown}
+        onClick={this.handleClick}
+      >
+        <span className="close" onClick={this.callOnClose}>
+          &times;
+        </span>
+
+        <span className="previous" onClick={() => this.changeImage(-1)}>
+          &#10094;
+        </span>
+
+        <span className="next" onClick={() => this.changeImage(1)}>
+          &#10095;
+        </span>
+
+        <div className="modal-content">
+          <div className="slide">
+            <img src={src[currentIndex]} alt=""/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
